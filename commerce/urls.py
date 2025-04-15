82efad651e9b16a0c06c1678d10a6ec724b89562
rhb181 <60511637+rhb181@users.noreply.github.com>
2025-04-15 22:28:57 +0100

    removed url from commerce and placed in auctions

--- commerce/urls.py
+++ commerce/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("auctions.urls")),
-    path("create", views.create, name="create")
-]
+    path("", include("auctions.urls"))
+    ]
