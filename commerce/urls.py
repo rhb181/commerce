fe0b25e67bce82b3383b4d3c0a2ea99ba1533611
rhb181 <60511637+rhb181@users.noreply.github.com>
2025-04-15 22:28:00 +0100

    add path to urls for create

--- commerce/urls.py
+++ commerce/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("auctions.urls"))
+    path("", include("auctions.urls")),
+    path("create", views.create, name="create")
 ]
